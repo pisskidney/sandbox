013da00ebbb68b9ba0547d81e13e7581752aef31
Peter Horvath <horvthpeter@gmail.com>
2014-09-08 15:23:54 +0000

    Added dp problem

---
+++ dp/graphs.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+
+'''
+Starting in the top left corner of a 2x grid, and only being able to move to
+the right and down, there are exactly 6 routes to the bottom right corner.
+'''
+
+def go(n):
+    dp = [[1 for i in range(n + 1)] for j in range(n + 1)]
+    for i in range(n + 1):
+        for j in range(n + 1):
+            if i - 1 >= 0 and j - 1 >= 0:
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+            elif i - 1 >= 0:
+                dp[i][j] *= dp[i-1][j] 
+            elif j - 1 >= 0:
+                dp[i][j] *= dp[i][j - 1]
+    print dp
+
+
+def main():
+    print go(20)
+
+
+if __name__ == "__main__":
+    main()
